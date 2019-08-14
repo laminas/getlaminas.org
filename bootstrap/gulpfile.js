f38bbec17699f8ffdfb2a8ddedce95a1c975ec34
Matthew Weier O'Phinney <matthew@zend.com>
2019-08-14 11:12:36 -0500

    feat: Platform.sh deployment configuration
    Adds configuration for deploying via Platform.sh. Choosing to run as
    php-fpm since it's easier to setup initially.
    
    Requires some updates to node packages and gulp configuration to run
    properly.
    
    TODO:
    
    - remove swoole from docker-compose configuration
    - remove Makefile

--- bootstrap/gulpfile.js
+++ bootstrap/gulpfile.js
@@ -12,18 +12,7 @@ gulp.task('sass', function () {
         .pipe(sourcemaps.init())
         .pipe(sass())
         .on('error', sass.logError)
-        .pipe(postcss([autoprefixer({
-            browsers: [
-                'Chrome >= 35',
-                'Firefox >= 38',
-                'Edge >= 12',
-                'Explorer >= 10',
-                'iOS >= 8',
-                'Safari >= 8',
-                'Android 2.3',
-                'Android >= 4',
-                'Opera >= 12']
-        })]))
+        .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'))
         .pipe(cleanCss())
