c0b63508a693104eeb16adf2b21a5f371a887213
James Titcumb <james@asgrim.com>
2022-07-15 07:37:02 +0100

    Import external vendor logos as pngs to avoid hotlinking
    Signed-off-by: James Titcumb <james@asgrim.com>

--- config/autoload/commercial-vendors.global.php
+++ config/autoload/commercial-vendors.global.php
@@ -6,13 +6,13 @@ return [
     'commercial-vendors' => [
         [
             'name' => 'Roave',
-            'logo' => 'http://ocramius.github.io/zf2-dpc-tutorial-slides/images/Roave_Logo_-_720p.png', // @todo don't hotlink
+            'logo' => '/images/vendors/roave.png',
             'url' => 'https://roave.com',
             'text' => 'Roave is a full-service web development firm, offering services such as consulting, training, software development, and more.',
         ],
         [
             'name' => 'Zend by Perforce',
-            'logo' => 'https://www.zend.com/sites/default/themes/custom/zend/logo.svg',
+            'logo' => '/images/vendors/zend.png',
             'url' => 'https://www.zend.com/products/laminas-enterprise-support',
             'text' => 'Zend is the PHP company, and offers training, services, and production support for Laminas and other PHP applications.',
         ],
