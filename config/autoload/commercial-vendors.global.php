2ae57bc08e314eeb895f8ab914db666ab28ed21d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-08-09 10:39:55 -0500

    feat: Apidemia (commercial vendor program)
    Adds Apidemia as a member of the commercial vendor program, using text, URL, and logo as provided by them.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- config/autoload/commercial-vendors.global.php
+++ config/autoload/commercial-vendors.global.php
@@ -7,14 +7,20 @@ return [
         [
             'name' => 'Roave',
             'logo' => '/images/vendors/roave.png',
-            'url' => 'https://roave.com',
+            'url'  => 'https://roave.com',
             'text' => 'Roave is a full-service web development firm, offering consulting, training, software development, and more.',
         ],
         [
             'name' => 'Zend by Perforce',
             'logo' => '/images/vendors/zend.png',
-            'url' => 'https://www.zend.com/products/laminas-enterprise-support',
+            'url'  => 'https://www.zend.com/products/laminas-enterprise-support',
             'text' => 'Zend is "The PHP Company", and offers training, services, and production support for Laminas and other PHP applications.',
+        ],
+        [
+            'name' => 'Apidemia',
+            'logo' => '/images/vendors/apidemia.png',
+            'url'  => 'https://www.apidemia.com',
+            'text' => 'Apidemia is a web development company, offering software development and consulting. Since 2005, we have focused on the PHP ecosystem, and our tools of trade have been Zend Framework 1, 2, and 3, and then Laminas and Mezzio. We created DotKernel, a collection of PSR-7 Middleware applications built on top of the Mezzio microframework and Laminas components.',
         ],
     ],
 ];
