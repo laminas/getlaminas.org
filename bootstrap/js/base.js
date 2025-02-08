02a4639c2d7c1be456c4a26aa006d28742a7f315
Frank Brückner <dev@froschdesignstudio.de>
2025-02-08 11:50:28 +0100

    Remove Anchor.js integration and related styles
    Anchor.js functionality and sticky header fixes have been removed from the project. This includes deleting associated SCSS styles, JavaScript code, and the package dependency in `package.json`. The cleanup helps simplify the codebase and remove unused features.
    
    Signed-off-by: Frank Brückner <dev@froschdesignstudio.de>

--- bootstrap/js/base.js
+++ bootstrap/js/base.js
@@ -1,12 +1,6 @@
 'use strict';
 
 {
-    // Anchors
-    anchors.options.placement = 'left';
-    anchors.add(
-        'article h1, article h2, article h3, article h4, article h5'
-    );
-
     // Pre elements - Prism code samples
     const preElements = document.querySelectorAll('pre');
     preElements.forEach(element => {
